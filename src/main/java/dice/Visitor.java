55a8c99cd4f510140f65768ae5cba4f6e8f7d234
willlowery <william.lowery@rocky.edu>
2017-02-01 17:06:12 -0700

    Add Basic Visitor

---
+++ src/main/java/dice/Visitor.java
@@ -0,0 +1,32 @@
+package dice;
+
+import generated.dice.DiceBaseVisitor;
+import generated.dice.DiceLexer;
+import generated.dice.DiceParser;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+public class Visitor {
+
+    public void run(String toParse) throws IOException {
+
+        ByteArrayInputStream input = new ByteArrayInputStream(toParse.getBytes());
+
+        new MyVisitor().visitParse(
+                new DiceParser(
+                        new CommonTokenStream(
+                                new DiceLexer(
+                                        new ANTLRInputStream(input)
+                                )
+                        )
+                ).parse()
+        );
+    }
+
+    private static class MyVisitor extends DiceBaseVisitor {
+
+    }
+}
